f34b30a69ed92d1ebc85adc6eff228f2379b13fa
Harsh-Topi <topiwalah@hotmail.com>
2020-09-04 23:33:08 -0400

    adjusted seo configuration

--- components/Head/head-seo-tags.jsx
+++ components/Head/head-seo-tags.jsx
@@ -9,7 +9,7 @@ function HeadOGGTags() {
 			/>
 			<meta
 				property="og:image"
-				content="https://drive.google.com/file/d/1MExbBfcmem8AdbYsfpuM2eaq6j2_PoXB/view?usp=sharing"
+				content="https://d0hx9a.dm.files.1drv.com/y4ms8WeFg5Gv4bSnO64D2KXi16IOr_axZblZh49QfqAcmPzPTPCawonLGkoyJZzQZ96Ad-kTur_fd0yXMbASeksPADmcdrdqwJzjaQexcwr6RBjuAid9GV3Nw8iagON48ui6HXHZf2QDtXlhRaas_zMU0L9Fpr3C_eKckgfkMLedPBo3HxJmFO_e8zqRrfkFUOHPPANNpS_Dd7Dfzo0rAdcLw?width=660&height=291&cropmode=none"
 			/>
 			<meta property="og:image:height" content="900" />
 			<meta property="og:image:width" content="900" />
