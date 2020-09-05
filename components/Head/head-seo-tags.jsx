4d692491b02a5c6c3c0f5fbbdc793cdf0cc6997d
Harsh-Topi <topiwalah@hotmail.com>
2020-09-04 23:01:25 -0400

    small adjustments

--- components/Head/head-seo-tags.jsx
+++ components/Head/head-seo-tags.jsx
@@ -1,13 +1,13 @@
 function HeadOGGTags() {
 	return (
 		<>
-			<meta property="og:site_name" content="My website" />
-			<meta property="og:title" content="Title of the website" />
-			<meta property="og:description" content="Description goes here" />
-			<meta property="og:image" content="image url" />
+			<meta property="og:site_name" content="" />
+			<meta property="og:title" content="Harsh Topiwala" />
+			<meta property="og:description" content="Just another portfolio." />
+			<meta property="og:image" content="https://ibb.co/QjtwCJQ" />
 			<meta property="og:image:height" content="1919" />
 			<meta property="og:image:width" content="789" />
-			<meta property="og:url" content="link to your webstie" />
+			<meta property="og:url" content="https://harshtopi.com/" />
 		</>
 	);
 }
