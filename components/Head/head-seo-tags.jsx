5fb94c0b6d0adab78fd592942d15b5b7b542dd29
Harsh-Topi <topiwalah@hotmail.com>
2020-09-04 23:25:04 -0400

    adjusted seo configuration

--- components/Head/head-seo-tags.jsx
+++ components/Head/head-seo-tags.jsx
@@ -7,9 +7,12 @@ function HeadOGGTags() {
 				property="og:description"
 				content="Harsh Topiwala is a Software Developer and student based out of Toronto, Ontario, Canada. Currently building projects and learning new things one step at a time."
 			/>
-			<meta property="og:image" content="https://ibb.co/QjtwCJQ" />
-			<meta property="og:image:height" content="1919" />
-			<meta property="og:image:width" content="789" />
+			<meta
+				property="og:image"
+				content="https://drive.google.com/file/d/1MExbBfcmem8AdbYsfpuM2eaq6j2_PoXB/view?usp=sharing"
+			/>
+			<meta property="og:image:height" content="900" />
+			<meta property="og:image:width" content="900" />
 			<meta property="og:url" content="https://harshtopi.com/" />
 		</>
 	);
