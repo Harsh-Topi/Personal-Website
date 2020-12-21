398175dd2d55e2b073083341cb43850dba9adfdd
Harsh-Topi <topiwalah@hotmail.com>
2020-12-20 20:11:59 -0500

    meta tags

--- components/Head/head-meta-tags.jsx
+++ components/Head/head-meta-tags.jsx
@@ -3,6 +3,19 @@ function HeadMetaTags() {
 		<>
 			<meta charSet="utf-8" />
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			<meta property="og:site_name" content="Harsh Topiwala" />
+			<meta property="og:title" content="Harsh Topiwala" />
+			<meta
+				property="og:description"
+				content="Harsh Topiwala is a Software Developer and student based out of Toronto, Ontario, Canada. Currently building projects and learning new things one step at a time."
+			/>
+			<meta
+				property="og:image"
+				content="https://eehx9a.dm.files.1drv.com/y4mgrHxBBue5sR73omRKMJGKtnA0tEFjWVSKpcKtNeIbXu9Ifr1ZF8FdM3Xe-yhq1g7ZJrafQGJkSGx0uik70CYWX7mlxqONitBatmEnw6OhEWsSZ5a3C_bGwJumFAxDOlZxj9Zo-0Jm0ThAk5WbWabRGz4opXE2rpTHzthSEDhhWb4w7IhyDZBv-lV3HM648O6cd32tmby041Btjj9mA-hSQ?width=500&height=500&cropmode=none"
+			/>
+			<meta property="og:image:height" content="500" />
+			<meta property="og:image:width" content="500" />
+			<meta property="og:url" content="https://harshtopi.com/" />
 		</>
 	);
 }
