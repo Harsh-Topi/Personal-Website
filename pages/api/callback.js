b210bcda79cc69329b269654b51a9a61216f6e11
Harsh <htopiwal@uoguelph.ca>
2022-05-22 16:59:19 -0400

    API - update

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,4 +1,5 @@
-/*global chrome*/
 export default function callback(req, res) {
+    var data = { type: "FROM_PAGE", text: "Hello from the webpage!" };
+    window.postMessage(data, "*");
     res.status(200).json({ name: 'John Doe' })
 }
